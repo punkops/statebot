d61c4aa2dc4e2ddb747b2fa35a63cdf58dcc169f
Nick Syntychakis <nsyntych@punkops.dev>
2024-05-27 01:32:40 +0300

    Tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,24 +2,28 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import pytest
 
-from statebot.main import aws
+from statebot.main import aws, main
 
 __author__ = "Nick Syntychakis"
 __copyright__ = "Nick Syntychakis"
 __license__ = "Apache-2.0"
 
 
-def test_aws(capsys):
-    # Call the aws function with the desired arguments
-    args = []  # Create an empty list to store the arguments
-    # Add your desired arguments to the list
-    args.append("arg1")
-    args.append("arg2")
-    aws(args)
+def test_aws():
+    """API Tests"""
+    # Setup
+    args = ["arg1", "arg2"]
+    # Verify
+    assert aws(args) is None
+    with pytest.raises(TypeError):
+        aws()
 
-    # Capture the stdout and stderr
-    captured = capsys.readouterr()
 
-    # Assert the expected output
-    assert "AWS Here!" in captured.out
+def test_main():
+    """API Tests"""
+    args = ["arg1", "arg2"]
+    with pytest.raises(TypeError):
+        main(args)
+    assert True
